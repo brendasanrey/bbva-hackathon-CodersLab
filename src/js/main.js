97371822df28833e7d3b1c134f5c9618efcb25c0
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-09-08 10:44:29 -0500

    Modificando json

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,15 @@
-var typeNumber = 4;
-var errorCorrectionLevel = 'L';
-var qr = qrcode(typeNumber, errorCorrectionLevel);
-qr.addData('Hola palmidia!');
-qr.make();
-document.getElementById('placeHolder').innerHTML = qr.createImgTag();
+
+window.getData = () =>{
+    fetch('https://raw.githubusercontent.com/sanrey254/bbva-hackathon-CodersLab/master/src/docs/data.json').then(result => result.json())
+    .then(result =>{
+        drawQrCode(result);
+    })
+}
+
+window.drawQrCode = (result) =>{ 
+    const resultToString = JSON.stringify(result);
+    const url =`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${resultToString}`;
+    console.log(url); 
+}
+
+getData();
