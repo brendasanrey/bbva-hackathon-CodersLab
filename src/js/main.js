e61d4767ac97070ca8a7d50df7a8e83196894c77
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-09-08 15:58:54 -0500

    Agregando vista login

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ window.drawQrCode = (result) =>{
     const resultToString = JSON.stringify(result);
     const url =`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${resultToString}`;
     console.log(url); 
+    document.getElementById('code').src = url;
 }
 
 getData();
