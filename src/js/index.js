fa4cce080cc813d376e79ef8e18646f07a876c65
Brenda Sánchez Reyes <sanrey254@hotmail.com>
2018-09-09 04:40:11 -0500

    Agregando contador de productos

--- src/js/index.js
+++ src/js/index.js
@@ -104,7 +104,7 @@ window.googleUserLogin = () => {
 window.signOutUser = () => {
   firebase.auth().signOut()
     .then(() => {
-      // Sign-out successful.
+      location.href = ('login.html');
     }).catch((error) => {
       // An error happened.
       console.log(error);
